c533c253ad45a57f54405fb87d97370c9de63251
hwiung <khw5233@gmail.com>
2024-12-04 18:24:24 +0900

    add: 주석, 에러코드

--- src/main/java/com/example/outsourcing/error/errorcode/ErrorCode.java
+++ src/main/java/com/example/outsourcing/error/errorcode/ErrorCode.java
@@ -9,8 +9,17 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    // 400 Bad Request
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+    // 401 Unauthorized
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
+
+    // 403 Forbidden
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN,"접근이 거부됐습니다."),
+
     // 404 NOT_FOUND
-    NOT_FOUNR_USER(HttpStatus.NOT_FOUND,"입력한 유저 ID를 찾을 수 없습니다.");
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND,"입력한 유저 ID를 찾을 수 없습니다.");
 
 
 
