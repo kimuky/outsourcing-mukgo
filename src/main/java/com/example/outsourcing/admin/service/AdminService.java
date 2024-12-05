67029a22e197d379ee517391cf3d2afeb71b399a
kimuky <xogus559@gmail.com>
2024-12-05 09:11:53 +0900

    add: 관리자 기능 구현에 따른 파일 생성

---
+++ src/main/java/com/example/outsourcing/admin/service/AdminService.java
@@ -0,0 +1,20 @@
+package com.example.outsourcing.admin.service;
+
+import com.example.outsourcing.admin.dto.StartEndDateDto;
+import com.example.outsourcing.admin.repository.AdminRepository;
+import com.example.outsourcing.entity.User;
+import com.example.outsourcing.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+    private final UserRepository userRepository;
+
+    public void getStatics(User loginUser, StartEndDateDto dateDto, Long storeId) {
+        userRepository.getDailyOrderCount();
+    }
+
+}
