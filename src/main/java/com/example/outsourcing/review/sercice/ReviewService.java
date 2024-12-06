e5833d8d04ee2b8e3df4346a060e3fd82abad86b
kimdanbin <rlaeksqls@naver.com>
2024-12-06 17:18:50 +0900

    update: 리뷰생성, 리뷰조회 예외상황 에러코드 추가

--- src/main/java/com/example/outsourcing/review/sercice/ReviewService.java
+++ src/main/java/com/example/outsourcing/review/sercice/ReviewService.java
@@ -9,6 +9,7 @@ import com.example.outsourcing.error.exception.CustomException;
 import com.example.outsourcing.order.repository.OrderRepository;
 import com.example.outsourcing.review.dto.ReviewResponseDto;
 import com.example.outsourcing.review.repository.ReviewRepository;
+import com.example.outsourcing.status.OrderStep;
 import com.example.outsourcing.store.repository.StoreRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,14 +24,30 @@ public class ReviewService {
     private OrderRepository orderRepository;
     private StoreRepository storeRepository;
 
-    // 리뷰 생성
+    /**
+     * 리뷰 생성
+     * @param user - 로그인 유저
+     * @param orderId - 주문 id
+     * @param comment - 코멘트
+     * @param rating - 별점
+     * @return 리뷰 아이디, 코멘트, 별점
+     */
     public ReviewResponseDto postReview(User user, Long orderId, String comment, int rating) {
 
         Orders order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new CustomException(ErrorCode.ORDER_NOT_FOUND));
 
-        Review review = new Review(user, order, comment, rating);
+        // 본인이 주문한 게 아닐때 예외
+        if (!order.getUser().getId().equals(user.getId())) {
+            throw new CustomException(ErrorCode.FORBIDDEN_ORDER);
+        }
 
+        // 배달완료가 아닐때 예외
+        if (!order.getStep().equals(OrderStep.DELIVERING_COMPLETED)) {
+            throw new CustomException(ErrorCode.NOT_DELIVERING_COMPLETED);
+        }
+
+        Review review = new Review(user, order, comment, rating);
         Review savedReview = reviewRepository.save(review);
 
         return ReviewResponseDto.builder()
