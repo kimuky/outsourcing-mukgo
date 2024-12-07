b4a3a95f0cdd5a4da968da7c03add368c23ce113
myoma <qq8380qq@gmail.com>
2024-12-07 16:58:00 +0900

    add : store 사장님 주문 목록 조회 및 주문 상태 변경 기능 구현

--- src/main/java/com/example/outsourcing/store/service/StoreService.java
+++ src/main/java/com/example/outsourcing/store/service/StoreService.java
@@ -1,14 +1,17 @@
 package com.example.outsourcing.store.service;
 
 import com.example.outsourcing.advertisement.repository.AdvertisementRepositoryImpl;
-import com.example.outsourcing.entity.Menu;
-import com.example.outsourcing.entity.Store;
-import com.example.outsourcing.entity.User;
+import com.example.outsourcing.entity.*;
 import com.example.outsourcing.error.errorcode.ErrorCode;
 import com.example.outsourcing.error.exception.CustomException;
 import com.example.outsourcing.menu.dto.MenuResponseDto;
+import com.example.outsourcing.menu.repository.MenuRepository;
+import com.example.outsourcing.order.dto.OrderDto;
+import com.example.outsourcing.order.dto.OrderResponseDto;
+import com.example.outsourcing.order.repository.OrderRepository;
 import com.example.outsourcing.status.Authority;
 import com.example.outsourcing.status.MenuStatus;
+import com.example.outsourcing.status.OrderStep;
 import com.example.outsourcing.status.StoreStatus;
 import com.example.outsourcing.store.dto.StoreMenuResponseDto;
 import com.example.outsourcing.store.dto.StoreRequestDto;
@@ -30,9 +33,12 @@ public class StoreService {
     private final StoreRepository storeRepository;
     public final UserRepository userRepository;
     private final AdvertisementRepositoryImpl advertisementRepositoryImpl;
+    private final OrderRepository orderRepository;
+    private final MenuRepository menuRepository;
 
     /**
      * 가게 생성
+     *
      * @param requestDto
      * @param loginUser
      * @return
@@ -40,18 +46,18 @@ public class StoreService {
     public StoreResponseDto createStore(StoreRequestDto requestDto, User loginUser) {
 
         //권한 검증
-        if(!loginUser.getAuthority().equals(Authority.OWNER)) {
+        if (!loginUser.getAuthority().equals(Authority.OWNER)) {
             throw new CustomException(ErrorCode.FORBIDDEN_PERMISSION);
         }
 
         //가게 수 확인
         long activeStoreCount = storeRepository.countByUserAndStatusNot(loginUser, StoreStatus.CLOSURE);
-        if(activeStoreCount >= 3) {
+        if (activeStoreCount >= 3) {
             throw new CustomException(ErrorCode.NOT_CREATE_STORE);
         }
 
         //오픈 시간, 마감 시간
-        if(requestDto.getOpenTime().isAfter(requestDto.getCloseTime())) {
+        if (requestDto.getOpenTime().isAfter(requestDto.getCloseTime())) {
             throw new CustomException(ErrorCode.OPEN_AND_CLOSE);
         }
 
@@ -69,6 +75,7 @@ public class StoreService {
 
     /**
      * 가게 수정
+     *
      * @param storeId
      * @param loginUserId
      * @param name
@@ -82,16 +89,16 @@ public class StoreService {
         Store findStore = storeRepository.findByStoreOrElseThrow(storeId);
 
         //가게 ID 확인
-        if(findStore.getId() == null) {
+        if (findStore.getId() == null) {
             throw new CustomException(ErrorCode.STORE_NOT_FOUND);
         }
 
         //본인의 가게 검증
-        if(!findStore.getUser().getId().equals(loginUserId)) {
+        if (!findStore.getUser().getId().equals(loginUserId)) {
             throw new CustomException(ErrorCode.FORBIDDEN_OWNER);
         }
 
-        if(name == null || minimumAmount == null || openTime == null || closeTime == null) {
+        if (name == null || minimumAmount == null || openTime == null || closeTime == null) {
             throw new CustomException(ErrorCode.BAD_REQUEST);
         }
 
@@ -103,7 +110,7 @@ public class StoreService {
 
     }
 
-//    /**
+    //    /**
 //     * 가게 단건 조회
 //     * @param storeId
 //     * @return
@@ -112,8 +119,8 @@ public class StoreService {
         // 가게 Id 확인
         Store findStore = storeRepository.findByStoreOrElseThrow(storeId);
 
-        if(findStore.getStatus() == StoreStatus.CLOSURE){
-            if(loginUser.getAuthority() != Authority.OWNER || !findStore.getUser().getId().equals(loginUser.getId())) {
+        if (findStore.getStatus() == StoreStatus.CLOSURE) {
+            if (loginUser.getAuthority() != Authority.OWNER || !findStore.getUser().getId().equals(loginUser.getId())) {
                 throw new CustomException(ErrorCode.STORE_NOT_FOUND);
             }
         }
@@ -137,12 +144,13 @@ public class StoreService {
 
     /**
      * 가게 이름 검색 조회 + 광고 우선 순위
+     *
      * @param name
      * @return
      */
     public List<StoreResponseDto> SearchStoreByName(String name, User loginUser) {
 
-        if(loginUser.getAuthority() != Authority.USER) {
+        if (loginUser.getAuthority() != Authority.USER) {
             return storeRepository.findByStoreAndNotUser(name, loginUser.getId());
         }
 
@@ -154,6 +162,7 @@ public class StoreService {
 
     /**
      * 가게 폐업 변경
+     *
      * @param storeId
      * @param userId
      */
@@ -163,7 +172,7 @@ public class StoreService {
         Store findStore = storeRepository.findByStoreOrElseThrow(storeId);
 
         //사용자 검증
-        if(!findStore.getUser().getId().equals(userId)){
+        if (!findStore.getUser().getId().equals(userId)) {
             throw new CustomException(ErrorCode.FORBIDDEN_PERMISSION);
         }
 
@@ -176,7 +185,8 @@ public class StoreService {
     }
 
     /**
-     *  본인 가게만 조회
+     * 본인 가게만 조회
+     *
      * @param loginUser
      * @return
      */
@@ -184,6 +194,124 @@ public class StoreService {
 
         // 본인 가게 조회
         return storeRepository.findByMyStore(loginUser.getId());
-
     }
+
+
+    /**
+     * 사장 본인 가게 주문 조회
+     * @param storeId
+     * @param loginUser
+     * @return
+     */
+    public List<OrderResponseDto> checkOrderStore(Long storeId, User loginUser) {
+        //가게 조회
+        Store findStore = storeRepository.findByStoreOrElseThrow(storeId);
+
+        //사용자 검증
+        if (!findStore.getUser().getId().equals(loginUser.getId())) {
+            throw new CustomException(ErrorCode.FORBIDDEN_PERMISSION);
+        }
+
+        // 가게에 들어온 주문 목록 조회
+        List<Orders> orders = orderRepository.findByStoreId(storeId);
+
+        List<OrderResponseDto> orderResponseDtos = new ArrayList<>();
+
+        for (Orders order : orders) {
+            // 주문 메뉴 조회
+            List<OrderMenu> orderMenus = order.getOrderMenu();
+            List<OrderDto> orderMenuDtos = new ArrayList<>();
+
+            for (OrderMenu orderMenu : orderMenus) {
+                String menuName = menuRepository.findById(orderMenu.getMenuId().getId())
+                        .orElseThrow(() -> new CustomException(ErrorCode.MENU_NOT_FOUND))
+                        .getMenuName();
+                Integer foodCount = orderMenu.getFoodCount();
+
+                orderMenuDtos.add(new OrderDto(orderMenu.getMenuId().getId(), menuName, foodCount));
+            }
+
+            // 주문 정보를 OrderResponseDto로 변환
+            OrderResponseDto orderResponseDto = OrderResponseDto.builder()
+                    .id(order.getId())
+                    .storeId(storeId)
+                    .order(orderMenuDtos)
+                    .totalPrice(order.getTotalPrice())
+                    .orderStep(order.getStep())
+                    .createdAt(order.getCreatedAt())
+                    .build();
+
+            orderResponseDtos.add(orderResponseDto);
+        }
+
+        return orderResponseDtos;
+    }
+
+
+    /**
+     * 주문 상태 변경
+     * @param storeId
+     * @param orderId
+     * @param loginUser
+     * @return
+     */
+    @Transactional
+    public OrderResponseDto updateOrderStatus(Long storeId, Long orderId, User loginUser) {
+        // 가게 소유자인지 검증
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new CustomException(ErrorCode.STORE_NOT_FOUND));
+
+        if (!store.getUser().getId().equals(loginUser.getId())) {
+            throw new CustomException(ErrorCode.FORBIDDEN_PERMISSION);
+        }
+
+        // 주문 조회
+        Orders order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new CustomException(ErrorCode.ORDER_NOT_FOUND));
+
+        // 주문이 해당 가게에 속하는지 확인
+        if (!order.getStore().getId().equals(storeId)) {
+            throw new CustomException(ErrorCode.ORDER_NOT_FOUND);
+        }
+
+        // 주문 상태 업데이트
+        OrderStep currentStep = order.getStep();
+        OrderStep nextStep = getNextOrderStep(currentStep);
+
+        order.setStep(nextStep);
+        orderRepository.save(order);
+
+        // OrderResponseDto 생성 및 반환
+        return OrderResponseDto.builder()
+                .id(order.getId())
+                .storeId(store.getId())
+                .orderStep(order.getStep())
+                .totalPrice(order.getTotalPrice())
+                .createdAt(order.getCreatedAt())
+                .build();
+    }
+
+
+    /**
+     * 순차적 상태 변화
+     * @param currentStep
+     * @return
+     */
+    private OrderStep getNextOrderStep(OrderStep currentStep) {
+        switch (currentStep) {
+            case ORDER_COMPLETED:
+                return OrderStep.ORDER_ACCEPTED;
+            case ORDER_ACCEPTED:
+                return OrderStep.COOKING;
+            case COOKING:
+                return OrderStep.COOKING_COMPLETED;
+            case COOKING_COMPLETED:
+                return OrderStep.DELIVERING;
+            case DELIVERING:
+                return OrderStep.DELIVERING_COMPLETED;
+            default:
+                throw new CustomException(ErrorCode.INVALID_ORDER_STEP);
+        }
+    }
+
 }
